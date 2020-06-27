64a45643d764674aff9a795b9905b96c267335dd
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-06-27 17:39:41 -0400

    added routes to  header links

--- sainikFrontend/src/App.js
+++ sainikFrontend/src/App.js
@@ -2,28 +2,38 @@ import React from "react";
 import "./App.css";
 import Login from "./views/Login/Login";
 import UserDetailsForm from "./views/UserDetailsForm/UserDetailsForm";
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
-import PensionDetails from "./views/UserDetailsForm/PensionDetails"
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import PensionDetails from "./views/UserDetailsForm/PensionDetails";
 import Test from "./components/Test/Test";
 import Registration from "./views/Registration/Registration";
 import UserMessage from "./components/UserMessage/UserMessage";
 import ZsbAdmin from "./views/ZsbAdmin/ZsbAdmin";
 import Search from "./views/Search/Search";
 import Header from "./components/Header/Header";
+import { Routes } from "./RouteConstants";
 
 function App() {
   return (
     <Router>
       <Switch>
         <div className="App">
-        <Header/>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/Form" component={UserDetailsForm} />
-          <Route exact path="/PensionDetails" component={PensionDetails} />
-          <Route exact path="/Reg" component={Registration} />
-          <Route exact path="/msg" component={UserMessage} />
-          <Route exact path="/zsbAdmin" component={ZsbAdmin} />
-          <Route exact path="/search" component={Search} />
+          <Header />
+          <Route exact path={Routes.login.url} component={Login} />
+          <Route exact path={Routes.userForm.url} component={UserDetailsForm} />
+          <Route exact path={Routes.reg.url} component={Registration} />
+          <Route
+            exact
+            path={Routes.userNotification.url}
+            component={UserMessage}
+          />
+          <Route exact path={Routes.zsbDashboard.url} component={ZsbAdmin} />
+          <Route exact path={Routes.search.url} component={Search} />
         </div>
       </Switch>
     </Router>
---
+++ sainikFrontend/src/RouteConstants.js
@@ -0,0 +1,34 @@
+export const Routes = {
+  reg: {
+    url: "/registration",
+    label: "Register",
+  },
+  home: {
+    url: "/",
+    label: "Home",
+  },
+  login: {
+    url: "/",
+    label: "Login",
+  },
+  search: {
+    url: "/search",
+    label: "Search",
+  },
+  zsbDashboard: {
+    url: "/zsbDashboard",
+    label: "Dashboard",
+  },
+  logout: {
+    url: "/logout",
+    label: "Logout",
+  },
+  userForm: {
+    url: "/userForm",
+    label: "User Form",
+  },
+  userNotification: {
+    url: "/userNotification",
+    label: "User Notification",
+  },
+};
