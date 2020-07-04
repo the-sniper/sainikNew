95c4691a40aa7752c49f09628fdc596dc150aafe
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-07-04 11:43:24 -0400

    added skeleton loading

--- sainikFrontend/src/views/Login/Login.js
+++ sainikFrontend/src/views/Login/Login.js
@@ -7,6 +7,33 @@ import { connect } from "react-redux";
 import { fetchUser } from "../../redux";
 import { Link } from "react-router-dom";
 import { Routes } from "../../RouteConstants";
+import ContentLoader, { Facebook } from "react-content-loader";
+
+const MyLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={500}
+    height={600}
+    viewBox="0 0 500 600"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="180" y="5" rx="3" ry="3" width="122" height="35" />
+    <rect x="200" y="55" rx="3" ry="3" width="82" height="30" />
+    <rect x="150" y="100" rx="3" ry="3" width="177" height="30" />
+
+    <rect x="0" y="180" rx="3" ry="3" width="80" height="20" />
+    <rect x="0" y="210" rx="3" ry="3" width="500" height="50" />
+
+    <rect x="0" y="280" rx="3" ry="3" width="80" height="20" />
+    <rect x="0" y="310" rx="3" ry="3" width="500" height="50" />
+
+    <rect x="0" y="380" rx="3" ry="3" width="150" height="25" />
+    <rect x="350" y="380" rx="3" ry="3" width="150" height="25" />
+
+    <rect x="0" y="480" rx="3" ry="3" width="500" height="50" />
+  </ContentLoader>
+);
 
 const mapStateToProps = (state) => {
   return {
@@ -78,18 +105,15 @@ class Login extends Component {
     ) {
       setTimeout(
         () => this.props.history.push(Routes.userNotification.url),
-        500
+        2000
       );
     } else if (
       this.props.state.user.userDetails.profileDetails.approvalStatus === "A" &&
       this.props.state.user.userDetails.profileDetails.userType === "ZSB"
     ) {
-      setTimeout(
-        () => this.props.history.push(Routes.zsbDashboard.url),
-        500
-      );
+      setTimeout(() => this.props.history.push(Routes.zsbDashboard.url), 2000);
     } else {
-      setTimeout(() => this.props.history.push(Routes.userForm.url), 500);
+      setTimeout(() => this.props.history.push(Routes.userForm.url), 2000);
     }
   };
 
@@ -100,75 +124,83 @@ class Login extends Component {
     }
     return (
       <div className="swLogin">
-        {/* <div className="logoContainer">
+        {/* {(user.loading = true)} */}
+        {user.loading ? (
+          <MyLoader />
+        ) : (
+          <>
+            {/* <div className="logoContainer">
           <img src="./images/logo.png" className="img-fluid" alt="" />
         </div> */}
-        <div className="loginGreet">
-          <h3>LOGIN</h3>
-          <p>Welcome</p>
-          <p>Login back to your account</p>
-        </div>
-        <div className="swLoginInputContainer">
-          <form id="LoginForm">
-            {user.error ? (
-              <div className="errorCnt">
-                <span class="material-icons">error</span>
-                <p>
-                  {user.error === "Unable to log in with provided credentials."
-                    ? "Username or Password Incorrect"
-                    : user.error}
-                </p>
-              </div>
-            ) : (
-              ""
-            )}
-            {user.userDetails.isAuthenticated ? (
-              <div className="successCnt">
-                <span class="material-icons">check_circle</span>
-                <p>User successfully logged in</p>
-              </div>
-            ) : (
-              ""
-            )}
-            {this.Login_Input().map((data, index) => (
-              <InputBox
-                label={data.inpLabel}
-                type={data.inpType}
-                id={`loginInp_${index}`}
-                placeholder={data.inpPlaceholder}
-                required={data.inpRequired}
-                minLength={data.minLength}
-                name={data.name}
-                changeHandler={this.changeHandler}
-                key={data.name}
-                errorText={data.errorText}
-              />
-            ))}
-            <div className="d-flex justify-content-between align-items-center">
-              <InputCheckbox label="Remember me" />
-              <a href="#">Forgot password?</a>
+            <div className="loginGreet">
+              <h3>LOGIN</h3>
+              <p>Welcome</p>
+              <p>Login back to your account</p>
             </div>
-            <PrimaryButton
-              type="submit"
-              label={
-                user.loading || user.userDetails.isAuthenticated
-                  ? [
-                      <object
-                        className="swBtnLoader"
-                        type="image/svg+xml"
-                        data="./images/three-dots.svg"
-                      />,
-                    ]
-                  : "LOGIN"
-              }
-              actionFunction={this.submitForm}
-            />
-          </form>
-          <p style={{ marginBottom: "5px", marginTop: "15px" }}>
-            Don't have an account yet ?
-          </p>
-          <Link to={Routes.reg.url}>Register here</Link>
-        </div>
+            <div className="swLoginInputContainer">
+              <form id="LoginForm">
+                {user.error ? (
+                  <div className="errorCnt">
+                    <span class="material-icons">error</span>
+                    <p>
+                      {user.error ===
+                      "Unable to log in with provided credentials."
+                        ? "Username or Password Incorrect"
+                        : user.error}
+                    </p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {user.userDetails.isAuthenticated ? (
+                  <div className="successCnt">
+                    <span class="material-icons">check_circle</span>
+                    <p>User successfully logged in</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {this.Login_Input().map((data, index) => (
+                  <InputBox
+                    label={data.inpLabel}
+                    type={data.inpType}
+                    id={`loginInp_${index}`}
+                    placeholder={data.inpPlaceholder}
+                    required={data.inpRequired}
+                    minLength={data.minLength}
+                    name={data.name}
+                    changeHandler={this.changeHandler}
+                    key={data.name}
+                    errorText={data.errorText}
+                  />
+                ))}
+                <div className="d-flex justify-content-between align-items-center">
+                  <InputCheckbox label="Remember me" />
+                  <a href="#">Forgot password?</a>
+                </div>
+                <PrimaryButton
+                  type="submit"
+                  label={
+                    user.loading || user.userDetails.isAuthenticated
+                      ? [
+                          <object
+                            className="swBtnLoader"
+                            type="image/svg+xml"
+                            data="./images/three-dots.svg"
+                          />,
+                        ]
+                      : "LOGIN"
+                  }
+                  actionFunction={this.submitForm}
+                />
+              </form>
+              <p style={{ marginBottom: "5px", marginTop: "15px" }}>
+                Don't have an account yet ?
+              </p>
+              <Link to={Routes.reg.url}>Register here</Link>
+            </div>
+          </>
+        )}
       </div>
     );
   }
